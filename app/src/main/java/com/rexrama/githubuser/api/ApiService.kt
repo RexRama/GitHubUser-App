d15eb9cd3120f97f2debd59d271f45970e8bf39d
RaidRama <raidhan.rama.11@gmail.com>
2024-03-18 13:41:55 +0700

    - Minor change

--- app/src/main/java/com/rexrama/githubuser/api/ApiService.kt
+++ app/src/main/java/com/rexrama/githubuser/api/ApiService.kt
@@ -1,7 +1,7 @@
 package com.rexrama.githubuser.api
 
-import com.rexrama.githubuser.data.GithubUser
 import com.rexrama.githubuser.data.GithubResponse
+import com.rexrama.githubuser.data.GithubUser
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
